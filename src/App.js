d610a2df6e826ae1c1fe5fde1876a77c9906ff62
Ananda Aditya <anandaadityaputra11@gmail.com>
2022-06-29 10:23:11 +0700

    membuat component card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Card from "./Components/Card";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Card />
     </div>
   );
 }
