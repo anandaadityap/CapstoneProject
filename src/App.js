e8fa618c9df29740fd8d415f36dee40104b3ca2d
Ananda Aditya <anandaadityaputra11@gmail.com>
2022-07-02 19:59:59 +0700

    membuat data dan konsum data cardMateri

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import jam from "./assets/home_kit/jam.png";
 import Login from "./Page/Login";
 import Home from "./Page/Home";
 import SignUp from "./Page/SignUp";
+import CardMateri from "./Components/Card/CardMateri";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <Login /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <CardMateri />
     </div>
   );
 }
--- src/Components/Card/CardStart.js
+++ src/Components/Card/CardStart.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import "./CardStart.css"
+import React from "react";
+import "./CardStart.css";
 
-const CardStart = (props) =>{
-    const {src} = props
-    return(
-        <div class="card text-center">
-        <img src={src} class="card-img" alt="gambar"></img>
-        <div class="card-img-overlay position-absolute top-50 start-50 translate-middle ">
-            <div className="tengah">
-                <h5 class="card-title ">Mencari</h5>
-                <a href="#" class="btn btn-secondary">Start Now</a>
-            </div>
+const CardStart = (props) => {
+  const { src } = props;
+  return (
+    <div class="card text-center">
+      <img src={src} class="card-img" alt="gambar"></img>
+      <div class="card-img-overlay position-absolute top-50 start-50 translate-middle ">
+        <div className="tengah">
+          <h5 class="card-titlee ">Mencari</h5>
+          <a href="#" class="btn btn-secondary">
+            Start Now
+          </a>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CardStart;
